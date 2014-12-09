5595ade2ea36a066550e9cc383aca848784dca50
Chris Lynch <chrislynch42@yahoo.com>
2014-12-08 20:53:24 -0500

    Add class to read service xml and return its attributes as a hash of hashes

---
+++ lib/r_liferay_tool/read_service.rb
@@ -0,0 +1,52 @@
+
+module RLiferayTool
+  class ReadService
+    attr_reader :service_xml_file, :entities
+
+    def initialize(service_xml_file)
+      self.service_xml_file=service_xml_file
+      self.entities = Hash.new()
+      read_xml
+    end
+
+
+
+    private
+    attr_writer :service_xml_file, :entities
+
+    def read_xml
+      xml_utility = XMLUtility.new(self.service_xml_file)
+      entity_nodes = xml_utility.xml_content.xpath('/service-builder/entity')
+
+      entity_nodes.each { | entity_node |
+        entity_name = entity_node.attr('name')
+        self.entities[entity_name] = Hash.new()
+        self.entities[entity_name]['columns'] = Hash.new()
+        read_columns(entity_name, entity_node)
+      }
+    end
+
+    def read_columns(entity_name, entity_node)
+      column_nodes = entity_node.xpath('./column')
+      column_nodes.each { | column_node|
+        column_name = column_node.attr('name')
+        if column_node.attr("primary") == 'true'
+          self.entities[entity_name]['primary_key'] = column_name
+        elsif !IGNORE_COLUMNS.include?(column_name)
+          self.entities[entity_name]['columns'][column_name] = column_node.attr('type')
+        end
+      }
+    end
+
+    IGNORE_COLUMNS = {
+        'groupId' => 'long',
+        'companyId' => 'long',
+        'userId' => 'long',
+        'userName' => 'long',
+        'createDate' => 'long',
+        'modifiedDate' => 'long'
+    }
+
+
+  end
+end
---
+++ test/r_liferay_tool/test_read_service.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require_relative '../test_helper'
+require_relative '../../lib/r_liferay_tool/read_service'
+
+module RLiferayTool
+  class TestReadService < Minitest::Test
+
+    def setup
+      @test_object = ReadService.new(TestFiles::SERVICE_XML)
+      @test_class_constant = ReadService
+    end
+
+    def teardown
+    end
+
+    def test_entities
+      entity_name = 'ChatResponse'
+
+      assert_equal(true,@test_object.entities.include?(entity_name),"Did not find Foo entity")
+      assert_equal('responseId',@test_object.entities[entity_name]['primary_key'],"Did not find primary key responseId")
+      column_name='title'
+      assert_equal(true,@test_object.entities[entity_name]['columns'].include?(column_name),"Did not find column #{column_name}")
+      assert_equal('String',@test_object.entities[entity_name]['columns'][column_name],"Did not find column #{column_name}'s type which is String")
+
+      column_name='companyId'
+      assert_equal(false,@test_object.entities[entity_name]['columns'].include?(column_name),"Did not find column #{column_name}")
+
+    end
+
+  end
+end
